20f48df57a5a448ff4b92792b4c6226ca6043fc8
seasons986 <annalin12345@gmail.com>
2023-06-25 21:20:13 -0400

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+  let wOffset = window.innerWidth/6;
+  let hOffset = window.innerHeight/6;
+  let w = window.innerWidth - (wOffset);
+  let h = window.innerHeight - (hOffset);
+  const parent = document.getElementById("parent");
+  const heading = document.getElementById("heading");
+  let button = document.createElement("button");
+  button.innerText = "🐹"
+  
+  reposition(button);
+  parent.appendChild(button);
+  
+  button.onclick = () => {
+    const hole = document.createElement("p");
+    hole.innerText = "🕳";
+    hole.style.top = button.style.top;
+    hole.style.left = button.style.left;
+    heading.innerHTML = `<h1>:)</h1>`;
+    button.style.display = "none";
+    parent.appendChild(hole);
+    setTimeout(() => {reposition(button)}, 700);
+  };
+
+  function reposition(elem) {
+    heading.innerHTML = `<h1>:(</h1>`;
+    elem.style.display = "block";
+    elem.style.top = `${Math.floor(getRandomInt(w),wOffset)}px`;
+    elem.style.left = `${Math.floor(getRandomInt(h),hOffset)}px`;
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+});
